f597321329f9a20cbea4259d486b999247f98282
TheNiv <nivai2233@gmail.com>
2020-09-28 12:38:58 +0300

    Update IVisitManagement.h

--- Header Files/IVisitManagement.h
+++ Header Files/IVisitManagement.h
@@ -1,10 +1,11 @@
 #pragma once
+#include<Visit.h>
 class IVisitManagement
 {
-	virtual bool EnoughSpaceFor(Visit visit) = 0;
-	virtual void BookVisit(Visit visit) = 0;
+	virtual bool EnoughSpaceFor(Visit& visit) = 0;
+	virtual void BookVisit(Visit& visit) = 0;
 	virtual void UpdateVisits() = 0;
-	virtual bool CanBook(Visit visit) = 0;
-	virtual double GetVisitCost(Visit visit) = 0;
+	virtual bool CanBook(Visit& visit) = 0;
+	virtual double GetVisitCost(Visit& visit) const = 0;
 
 };
