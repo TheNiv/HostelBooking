a755e3ca1087fb4a4e0367ae841df1dc574bc017
TheNiv <nivai2233@gmail.com>
2020-09-29 21:44:31 +0300

    Some changes
    Removed redundant include

--- Header Files/Guest.h
+++ Header Files/Guest.h
@@ -1,12 +1,11 @@
 #pragma once
-#include "Visit.h"
 
 class Guest
 {
 private:
-	const char* m_name;  // the name of the guest
-	int m_age;  // the age of the guest
-	const char* m_id; // the id of the guest
+	const char* m_name;  // The name of the guest
+	int m_age;  // The age of the guest
+	const char* m_id; // The id of the guest
 
 public:
 	Guest(const char* name, int age, const char* id);
