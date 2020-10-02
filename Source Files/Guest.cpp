46271ac484682b17641ab3b036f16f0ff6443ebc
TheNiv <nivai2233@gmail.com>
2020-10-02 09:37:10 +0300

    << operator overload and getters added

--- Source Files/Guest.cpp
+++ Source Files/Guest.cpp
@@ -1,16 +1,33 @@
 #include "Guest.h"
 
-Guest::Guest(const char* name,int age,const char* id)
+Guest::Guest(const char* name, int age, const char* id) : m_name(name), m_age(age),m_id(id)
 {
-	Guest::m_name = name;
-	Guest::m_age = age;
-	Guest:m_id = id;
+
 }
-Guest::Guest() {
-	Guest::m_name = "";
-	Guest::m_age = 0;
+Guest::Guest() : m_name(""),m_age(0)
+{
 }
 
 Guest::~Guest()
 {
+}
+const char* Guest::GetName() 
+{
+	return m_name;
+}
+int Guest::GetAge() 
+{
+	return m_age;
+}
+const char* Guest::GetId()
+{
+	return m_id;
+}
+
+std::ostream& operator<< (std::ostream& os , Guest guest)
+{
+	os<< "name:" << guest.GetName() << std::endl;
+	os << "Age:" << guest.GetAge() << std::endl;
+	os<< "Id:" << guest.GetId() << std::endl;
+	return os;
 }
