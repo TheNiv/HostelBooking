be17c7172a40379405bd80a1ddf3ffcba133c802
TheNiv <nivai2233@gmail.com>
2020-10-02 09:39:25 +0300

    Added operator overloading

--- Header Files/Date.h
+++ Header Files/Date.h
@@ -3,38 +3,67 @@
 #include <ctime>
 struct Date
 {
-	int day;
-	int month;
-	int year;
-	Date(int d,int m,int y) {
-		day = d;
-		month = m;
-		year = y;
+
+	int m_day;  // The number of the day in the month
+	int m_month;  // The number of the month in the year
+	int m_year;  // The year
+	Date(int d, int m, int y) : m_day(d),m_month(m), m_year(y)
+	{
+
 	}
-	Date() {
-		day = 0;
-		month = 0;
-		year = 0;
+	Date() : m_day(0), m_month(0) , m_year(0)
+	{
+
 	}
 
-	static int CalculateDays(Date& start, Date& end) {
-		int sum = abs((end.year - start.year) * 360 + (end.month - start.month) * 30 + (end.day - start.day));
+	static int CalculateDays(Date& start, Date& end)  // O(1)
+	{  //Calculates the amount of days between 2 dates.
+		int sum = abs((end.m_year - start.m_year) * 360 + (end.m_month - start.m_month) * 30 + (end.m_day - start.m_day));
 		return sum;
 	}
-	bool IsAfter(Date& date) {
-		if (year < date.year) {
+	bool IsAfter(Date& other) const // O(1)
+	{  // Returns true if this date is after the date other and false otherwise.
+		if (m_year < other.m_year) 
+		{
 			return false;
 		}
-		else if (month < date.month && year == date.year)
+		else if (m_month < other.m_month && m_year == other.m_year)
 			return false;
-		else if (day < date.day && month == date.month && year == date.year)
-			return false;			
+		else if (m_day < other.m_day && m_month == other.m_month && m_year == other.m_year)
+			return false;
 		return true;
 	}
-	bool Equals(Date& d) 
-	{  //returns true if the dates are the same, false otherwise.
-		if (day == d.day && month == d.month && year == d.year)
+	bool Equals(Date& other) const // O(1)
+	{  //Returns true if the dates are the same, and false otherwise.
+		if (m_day == other.m_day && m_month == other.m_month && m_year == other.m_year)
 			return true;
 		return false;
+	}
+	bool IsBefore(Date& other) const
+	{   //Returns true if this date is before the date other and false otherwise.
+		if (m_year > other.m_year)
+			return false;
+		else if (m_month > other.m_month && m_year == other.m_year)
+			return false;
+		else if (m_day > other.m_day && m_month == other.m_month && m_year == other.m_year)
+			return false;
+		return true;
+	}
+	friend std::ostream& operator<< (std::ostream& os, const Date& date)
+	{
+		os << date.m_day << "." << date.m_month << "." << date.m_year;
+		return os;
+	}
+	bool operator>(Date& other) const
+	{
+		return IsAfter(other);
+	}
+	bool operator==(Date& other) const 
+	{
+		return Equals(other);
+	}
+	bool operator<(Date& other) const
+	{
+		return IsBefore(other);
 	}
 };
