3fbd52c6dace5e6d0f8ff755f9a871606bc557ca
TheNiv <nivai2233@gmail.com>
2020-10-07 11:08:40 +0300

    Removed a function that is not used.

--- Header Files/IVisitManagement.h
+++ Header Files/IVisitManagement.h
@@ -1,10 +1,9 @@
 #pragma once
-#include<Visit.h>
+#include "Visit.h"
 class IVisitManagement
 {
-	virtual bool EnoughSpaceFor(Visit& visit) = 0;
+	virtual bool EnoughSpaceFor(Visit& visit) const = 0;
 	virtual void BookVisit(Visit& visit) = 0;
-	virtual void UpdateVisits() = 0;
 	virtual bool CanBook(Visit& visit) = 0;
 	virtual double GetVisitCost(Visit& visit) const = 0;
 
